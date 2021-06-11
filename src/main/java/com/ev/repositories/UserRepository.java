e5fc6f2504e0066734c76f268244ef763b7df694
dhanush013 <dhan132000@gmail.com>
2021-06-11 17:29:58 +0530

    ..

--- src/main/java/com/ev/repositories/UserRepository.java
+++ src/main/java/com/ev/repositories/UserRepository.java
@@ -1,5 +1,12 @@
 package com.ev.repositories;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.ev.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	
 }
