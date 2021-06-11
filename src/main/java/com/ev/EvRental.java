9c65041f5b38ac1a244460dccef8d9fa77d32c89
dhanush013 <dhan132000@gmail.com>
2021-06-11 15:25:35 +0530

    Initial commit

---
+++ src/main/java/com/ev/EvRental.java
@@ -0,0 +1,13 @@
+package com.ev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvRental {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EvRental.class, args);
+	}
+
+}
