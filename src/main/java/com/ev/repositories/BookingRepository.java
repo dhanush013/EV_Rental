89759381ae4e91385eaaec4a2ac592c297cb48e1
dhanush013 <dhan132000@gmail.com>
2021-06-17 11:38:07 +0530

    Update BookingRepository.java

--- src/main/java/com/ev/repositories/BookingRepository.java
+++ src/main/java/com/ev/repositories/BookingRepository.java
@@ -4,7 +4,6 @@ package com.ev.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ev.entities.Booking;
@@ -14,7 +13,7 @@ import com.ev.entities.User;
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
 	List<Booking> findByUser(User user) ;
-	
-	@Query("select count(cars from booking (userId=Distinct))")
 	Booking findById(int bookingId) ;
+	@Query("select count(cars from booking (userId=Distinct))");
+	
 	}
